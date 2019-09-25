81cb0463320a5883f97b2bfa0934fec23a8e76dc
chris <iiscocoyt@gmail.com>
2019-09-25 14:58:32 -0700

    Added socket headers for both windows and linux

--- client/main.cpp
+++ client/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+#ifdef __linux__
+  #include <arpa/inet.h>
+#elif _WIN32
+  #include <Winsock2.h>
+#elif __APPLE__
+  //mac sock file
+#else
+  //error
+#endif
+
 using namespace std;
 
 int main() {
